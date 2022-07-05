7781eb9314ec3d445fa924f0760353cc844d692b
_MT <67404745+MTxSouza@users.noreply.github.com>
2022-07-05 15:51:53 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version="0.1.0",
     description="A demo which extract information from driving license.",
     author="BRAIN (Brazilian Artificial Inteligence Nuclues)",
-    packages=["library/extra_functions", "library/models"],
     license="LICENSE.txt",
     long_description=open("README.md").read(),
     install_requires=[
